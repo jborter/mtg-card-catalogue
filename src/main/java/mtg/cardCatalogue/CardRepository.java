eab81b747985afa8888d67adb7ad2f00c313a0d0
jborter <juerg.borter@zuehlke.com>
2016-06-23 11:55:54 +0200

    improve price updating

--- src/main/java/mtg/cardCatalogue/CardRepository.java
+++ src/main/java/mtg/cardCatalogue/CardRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -20,6 +22,10 @@ import java.util.List;
  */
 @RepositoryRestResource(collectionResourceRel = "cards", path = "cards")
 public interface CardRepository extends MongoRepository<Card, String>{
+
+    @RestResource(path="findByName", rel = "findByName")
     public List<Card> findByNameIgnoreCaseLike(@Param("name") String name);
+
+    @RestResource(path = "findByNameIgnoreCaseLike", rel = "findByNameIgnoreCaseLike")
     public Page<Card> findByNameIgnoreCaseLike(@Param("name") String name, Pageable page);
 }
